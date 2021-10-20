d75ad7f7edf9b3a2026cb879bbfe5777aaf6ad27
amanda taylor <amandactaylor@gmail.com>
2021-10-20 14:36:31 -0700

    max prime tests and results

---
+++ cpu_prime.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo -n > cpu_test.txt
+for i in 1 2 3 4 5; do sysbench --test=cpu --cpu-max-prime=100000 --num-threads=1 --max-time=45 run >> cpu_test.txt; done
+for i in 1 2 3 4 5; do sysbench --test=cpu --cpu-max-prime=100000 --num-threads=2 --max-time=45 run >> cpu_test.txt; done
+for i in 1 2 3 4 5; do sysbench --test=cpu --cpu-max-prime=100000 --num-threads=8 --max-time=45 run >> cpu_test.txt; done
+echo finished
