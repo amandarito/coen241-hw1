531da1ec73176fb771e9deff58fb056998132880
amanda taylor <amandactaylor@gmail.com>
2021-10-21 04:27:01 +0000

    fixed test

--- cpu_prime.sh
+++ cpu_prime.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo -n > cpu_test.txt
-for i in 1 2 3 4 5; do sysbench --test=cpu --cpu-max-prime=100000 --num-threads=1 --max-time=45 run >> cpu_test.txt; done
-for i in 1 2 3 4 5; do sysbench --test=cpu --cpu-max-prime=100000 --num-threads=2 --max-time=45 run >> cpu_test.txt; done
-for i in 1 2 3 4 5; do sysbench --test=cpu --cpu-max-prime=100000 --num-threads=8 --max-time=45 run >> cpu_test.txt; done
+for i in 1 2 3 4 5; do sysbench --test=cpu --cpu-max-prime=5000 --num-threads=1 --max-time=45 run >> cpu_test.txt; done
+for i in 1 2 3 4 5; do sysbench --test=cpu --cpu-max-prime=5000 --num-threads=2 --max-time=45 run >> cpu_test.txt; done
+for i in 1 2 3 4 5; do sysbench --test=cpu --cpu-max-prime=5000 --num-threads=8 --max-time=45 run >> cpu_test.txt; done
 echo finished
+
